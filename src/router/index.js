29d0d6bc24f302e7d4f0d5f326eadf317c2100f8
Рощупкин Виталий <elpistolero1999@gmail.com>
2021-01-17 15:51:58 +0300

    create path

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,52 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Home from "../views/Home";
+import Ad from "../views/Ads/Ad";
+import NewAd from "../views/Ads/NewAd";
+import AdList from "../views/Ads/AdList";
+import Orders from "../views/Orders/Orders";
+import Login from "../views/Auth/Login";
+import Registration from "../views/Auth/Registration";
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: '',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/ad:id',
+    name: 'ad',
+    component: Ad
+  },
+  {
+    path: '/new-ad',
+    name: 'newAd',
+    component: NewAd
+  },
+  {
+    path: '/user-ad',
+    name: 'adList',
+    component: AdList
+  },
+  {
+    path: '/orders',
+    name: 'orders',
+    component: Orders
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/registration',
+    name: 'registration',
+    component: Registration
+  },
+];
 
 const router = new VueRouter({
   mode: "history",
