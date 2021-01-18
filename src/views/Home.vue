b4e444ab7740fcb4e57992c81395257473cff40d
Рощупкин Виталий <elpistolero1999@gmail.com>
2021-01-18 23:26:48 +0300

    create orders page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,67 @@
 <template>
-    <v-container fluid>
-        <v-layout row>
-            <v-flex xs12>
-                <v-carousel>
-                    <v-carousel-item
-                            v-for="ad in ads"
-                            :key="ad.id"
-                            :src="ad.imgSrc"
+    <div>
+        <v-container fluid>
+            <v-layout row>
+                <v-flex xs12>
+                    <v-carousel>
+                        <v-carousel-item
+                                v-for="ad in ads"
+                                :key="ad.id"
+                                :src="ad.imgSrc"
+                        >
+                            <div class="ad-tran">
+                                <v-btn :to="'/ad/' + ad.id" elevation="0" class="error" >{{ ad.title }}</v-btn>
+                            </div>
+                        </v-carousel-item>
+                    </v-carousel>
+                </v-flex>
+            </v-layout>
+        </v-container>
+        <v-container mt-5 grid-list-lg>
+            <v-layout row>
+                <v-flex
+                        xs12
+                        md6
+                        lg4
+                        v-for="ad in ads"
+                        :key="ad.id"
+                >
+                    <v-card
+                            class="mx-auto"
                     >
-                        <v-btn :to="'/ad/' + ad.id" elevation="0" class="ad-tran">{{ ad.title }}</v-btn>
-                    </v-carousel-item>
-                </v-carousel>
-            </v-flex>
-        </v-layout>
-    </v-container>
+                        <v-img
+                                :src="ad.imgSrc"
+                                height="300px"
+                        ></v-img>
+
+                        <v-card-title>
+                            {{ ad.title }}
+                        </v-card-title>
+
+                        <v-card-subtitle>
+                            {{ ad.description }}
+                        </v-card-subtitle>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                    :to="'/ad/' + ad.id"
+                                    elevation="1"
+                            >
+                                open
+                            </v-btn>
+                            <v-btn
+                                    color="primary"
+                                    raised
+                            >
+                                buy
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
 </template>
 
 <script>
@@ -52,10 +100,11 @@
 <style scoped>
     .ad-tran {
         position: absolute;
-        right: 47%;
+        left: 50%;
         bottom: 50px;
-        opacity: 0.5;
-        color: white;
+        transform: translate(-50%, 0);
+        background: rgba(0, 0, 0, 0.3);
+        padding: 10px 20px;
 
     }
 </style>
--- src/views/Orders/Orders.vue
+++ src/views/Orders/Orders.vue
@@ -1,8 +1,26 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>Orders</h1>
+            <v-flex xs12 sm8 offset-sm2 md6 offset-md3>
+                <h1 class="text--secondary" mb-3>Orders</h1>
+                <v-list
+                        flat
+                        subheader
+                        three-line
+                >
+                    <v-list-item v-for="order in orders" :key="order.id">
+                            <v-list-item-action>
+                                <v-checkbox :input-value="order.done" @change="changeDone(order)"></v-checkbox>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                                <v-list-item-title>{{ order.name }}</v-list-item-title>
+                                <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+                            </v-list-item-content>
+                            <v-list-item-action>
+                                <v-btn :to="'/ad/' + order.adId" class="primary">open</v-btn>
+                            </v-list-item-action>
+                    </v-list-item>
+                </v-list>
             </v-flex>
         </v-layout>
     </v-container>
@@ -12,7 +30,21 @@
     export default {
         data () {
             return {
-
+                orders: [
+                    {
+                        id: 'n1',
+                        name: 'Vitan',
+                        phone: '+375255552288',
+                        adId: '1',
+                        done: false
+                    }
+                ]
+            }
+        },
+        methods: {
+            changeDone (order) {
+                order.done = true;
+                console.log('done')
             }
         }
     }
