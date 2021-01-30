8ea0ccda6e40685ffcf7670b552ef54dad07971b
Рощупкин Виталий <elpistolero1999@gmail.com>
2021-01-30 22:41:44 +0300

    create ad page

--- src/views/Ads/Ad.vue
+++ src/views/Ads/Ad.vue
@@ -1,8 +1,44 @@
 <template>
-    <v-container>
+    <v-container mt-10>
         <v-layout row>
             <v-flex xs12>
-                <h1>Ad</h1>
+                <v-card
+                        class="mx-auto"
+                        max-width="600px"
+                        elevation="12"
+                >
+                    <v-img
+                            class="white--text align-end"
+                            height="300px"
+                            src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+                    >
+                    </v-img>
+
+                    <v-card-subtitle class="pb-0">
+                        Lorem
+                    </v-card-subtitle>
+
+                    <v-card-text class="text--primary">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, sint?
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                                color="orange"
+                                depressed
+                        >
+                            Edit
+                        </v-btn>
+
+                        <v-btn
+                                color="success"
+
+                        >
+                            Buy
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
